c4e916f14d8008f77a1f16e1a81072a6e896d4d3
Vu Duc Tuyen <kan.vuduc@gmail.com>
2016-07-31 14:32:38 +0700

    Demo on ItemClick with events and command  binding

---
+++ ListViewQs/ListViewQsPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace ListViewQs
+{
+	public partial class ListViewQsPage : ContentPage
+	{
+		public ListViewQsPage()
+		{
+			InitializeComponent();
+
+			var vm = new ListViewQsViewModel()
+			{
+				Items = new ListItemModel[] { 
+					new ListItemModel {
+						Title = "Title 1"
+					},
+					new ListItemModel {
+						Title = "Title 2"
+					},
+					new ListItemModel {
+						Title = "Title 3"
+					},
+					new ListItemModel {
+						Title = "Title 4"
+					},
+					new ListItemModel {
+						Title = "Title 5"
+					},
+					new ListItemModel {
+						Title = "Title 6"
+					},
+					new ListItemModel {
+						Title = "Title 7"
+					}
+				}
+			};
+
+			BindingContext = vm;
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			var clickedButton = sender as Button;
+
+			var item = clickedButton.BindingContext as ListItemModel;
+
+			//Handle item clicked on button inside template
+		}
+	}
+
+	public class ListViewQsViewModel
+	{
+		public ListItemModel[] Items
+		{
+			get;
+			set;
+		}
+
+		public Command<ListItemModel> ItemClickCommand { 
+			get {
+				return new Command<ListItemModel>((item) => { 
+					//Handle item clicked on button inside template
+				});
+			}
+		}
+	}
+
+	public class ListItemModel
+	{
+		public string Title
+		{
+			get;
+			set;
+		}
+	}
+}
+
